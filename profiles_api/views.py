5e1273e10bbcd94f1864953cf97039fb6b86b778
dextrous-avi <avinash8946@gmail.com>
2020-04-05 20:06:36 +0530

    added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,10 +2,64 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 # Create your views here.
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a hello message."""
+        ...
+
+    def create(self, request):
+        """Create a new hello message."""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """Handle getting an object by its ID"""
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+
+        return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle craeting and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+
 
 class HelloApiView(APIView):
     """Test API view"""
@@ -33,13 +87,12 @@ class HelloApiView(APIView):
 
     def put(self,request,pk=None):
         """Handle updating an object"""
-        return Response({'method':'PUT'})
+        return Response({'http_method':'PUT'})
 
     def patch(self,request,pk=None):
         """Handle partial update of an object"""
-        return Response({'method':'PATCH'})
+        return Response({'http_method':'PATCH'})
 
     def delete(self,request,pk=None):
         """Delete an object"""
-        return Response({'method':'DELETE'})
-        
+        return Response({'http_method':'DELETE'})
